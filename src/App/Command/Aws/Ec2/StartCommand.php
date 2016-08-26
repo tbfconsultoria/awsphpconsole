36e2942cb3ac116f3112d9fa6a68115853fea8d0
Caio Marcelo Nardi <caio.nardi@tbfconsultoria.com.br>
2016-08-26 13:47:57 -0300

    Comandos start e stop aceitando array

--- src/App/Command/Aws/Ec2/StartCommand.php
+++ src/App/Command/Aws/Ec2/StartCommand.php
@@ -5,8 +5,8 @@ namespace App\Command\Aws\Ec2;
 //use Aws\Credentials\CredentialProvider;
 use Aws\Ec2\Ec2Client;
 use Knp\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -20,7 +20,7 @@ class StartCommand extends Command
             ->setName('app:aws-ec2:start')
             // the short description shown while running "php bin/console list"
             ->setDescription('Inicia uma instancia.')
-            ->addArgument('instanceId', InputArgument::REQUIRED, 'Qual instancia parar?');
+            ->addArgument('instanceId', InputArgument::IS_ARRAY, 'Qual instancia parar?');
     }
 
     protected function execute(InputInterface $input,
@@ -37,11 +37,14 @@ class StartCommand extends Command
         ]);
         try {
             $result = $result = $client->startInstances([
-                'InstanceIds' => [$instanceId]
+                'InstanceIds' => $instanceId
             ]);
-            print_r($result);
-        } catch (Exception $e) {
-            $output->writeln("#ERRO " . $e->getMessage());
+            foreach ($result['StartingInstances'] as $instance) {
+                $io->writeln($instance['InstanceId'] . ' - ' . $instance['CurrentState']['Name']);
+            }
+        } catch (\Exception $e) {
+            $io->error("verifique o ID da instancia\n" . implode("\n", $instanceId));
+            //$output->writeln("#ERRO " . $e->getMessage());
         }
     }
 }
